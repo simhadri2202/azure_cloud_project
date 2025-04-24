13967158f64b19250da1110ccd582a64b9f3385a
Simhadri Sriteja <simhadrisriteja@simhadris-air.wifi.uc.edu>
2025-04-24 12:37:10 -0400

    loginpage

---
+++ azure_flask_webapp/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    return f"User {username} with email {email} registered!"
+
+if __name__ == '__main__':
+    app.run()
